fe89185b4002d0026506f6cd27fcfb2438553a33
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2023-03-22 18:08:32 +0500

    Committing files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable linebreak-style */
 import React from 'react';
 import ReactDOM from 'react-dom';
-// eslint-disable-next-line import/no-extraneous-dependencies, quotes
-import "jquery";
-// eslint-disable-next-line import/no-extraneous-dependencies, quotes
-import "popper.js/dist/umd/popper";
-// eslint-disable-next-line quotes
-import "boostrap/dist/js/bootstrap";
-// eslint-disable-next-line quotes
-import "bootstrap/dist/css/boostrap.css";
+import 'jquery';
+import 'popper.js/dist/umd/popper';
+import 'boostrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/boostrap.css';
+import 'font-awesome/css/font-awesome.css';
+import './index.css';
 
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line no-unused-vars, linebreak-style
 const element = <div>Hello World</div>;
 ReactDOM.render(element, document.getElementById('root'));
-
-// eslint-disable-next-line no-console
-console.log(element);
