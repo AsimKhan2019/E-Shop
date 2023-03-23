bd6e9b72357ab16f14a17016ea1708bb2beef69c
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2023-03-23 19:23:49 +0500

    Fixed linter errors

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-var */
+/* eslint-disable linebreak-style */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable linebreak-style */
 /* eslint-disable react/button-has-type */
